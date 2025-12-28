60cb0f6a7413ce66d7a55d1e602a0f1f41eb36a3
ummheather <umm.heatherr@gmail.com>
2025-12-28 17:21:16 -0330

    edited intro cutscene

--- src/game/camera.c
+++ src/game/camera.c
@@ -1255,7 +1255,6 @@ s32 update_fixed_camera(struct Camera *c, Vec3f focus, UNUSED Vec3f pos) {
     focus[1] += focusFloorOff + 130.f;
     vec3f_get_dist_and_angle(focus, c->pos, &distCamToFocus, &faceAngle[0], &faceAngle[1]);
     faceAngle[2] = 0;
-    focus[0] -= 5.f;
 
     vec3f_copy(basePos, sFixedModeBasePosition);
     vec3f_add(basePos, sCastleEntranceOffset);
@@ -2638,6 +2637,7 @@ void update_camera(struct Camera *c) {
 
     update_lakitu(c);
 
+    
     gLakituState.lastFrameAction = sMarioCamState->action;
 }
 
@@ -5976,28 +5976,28 @@ BAD_RETURN(s32) cutscene_non_painting_death(struct Camera *c) {
 }
 
 BAD_RETURN(s32) cutscene_intro_init(struct Camera *c) {
-    c->pos[1] += 145.0f; // original value was 145
-    rotate_and_move_vec3f(c->pos, sMarioCamState->pos, -0x210, 0, 0x1A4);
+    c->pos[1] += 140.0f; // original value was 145
+    rotate_and_move_vec3f(c->pos, sMarioCamState->pos, -0x20A, 0, 0x1A4);
 }
 
 BAD_RETURN(s32) cutscene_intro_rotate_camera(struct Camera *c) {
     rotate_and_move_vec3f(c->pos, sMarioCamState->pos, 0, 0, -0x1C8);
 
     if (gCutsceneTimer > 60) {
-        c->pos[0] = -1528.0f;
+        c->pos[0] = -1525.0f;
     }
 }
 
 BAD_RETURN(s32) cutscene_intro_zoom(struct Camera *c) {
     rotate_and_move_vec3f(c->pos, sMarioCamState->pos, 0, 0x10, 0);
-    c->pos[1] += 0.265f;
-    c->pos[2] -= 0.850f;
+    c->pos[1] += 0.50f;
+    c->pos[2] -= 1.5f;
 }
 
 BAD_RETURN(s32) cutscene_intro(struct Camera *c) {
     cutscene_event(cutscene_intro_init, c, 0, 0);
     cutscene_event(cutscene_intro_rotate_camera, c, 0, 60);
-    cutscene_event(cutscene_intro_zoom, c, 60, 75);
+    cutscene_event(cutscene_intro_zoom, c, 62, 75);
 }
 
 BAD_RETURN(s32) cutscene_intro_end(struct Camera *c) {
